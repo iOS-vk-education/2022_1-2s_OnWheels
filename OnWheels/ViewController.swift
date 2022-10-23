d8985e8cb3fecb2b24fdb74e7de16a80d60f6588
Ilia Kosov <kosov11@gmail.com>
2022-10-23 13:31:39 +0300

    required .init project

---
+++ OnWheels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnWheels
+//
+//  Created by Илья on 10/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
