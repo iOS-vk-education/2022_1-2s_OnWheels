e981f63a26da8c2fa1a688d861cee6b9d1e94e63
Ryabova Veronika IU5-55B <70639339+nika-rvv@users.noreply.github.com>
2023-05-23 18:24:00 +0300

    add table adapter and model for view

---
+++ OnWheels/Events/Model/RaceInfo.swift
@@ -0,0 +1,19 @@
+//
+//  RaceInfo.swift
+//  OnWheels
+//
+//  Created by Veronika on 23.05.2023.
+//
+
+import Foundation
+struct RaceInfo: Hashable {
+    let id: Int
+    let title: String
+    let dateSubtitle: String
+    let placeName: String
+    let imageId: String
+    var numberOfLikes: Int
+    var numberOfParticipants: Int
+    var numberOfWatchers: Int
+    var isLiked: Bool
+}
